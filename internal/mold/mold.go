02c811d22f85168ac3b13eaafc1d8113f6b9abc2
Brandon Davis <dbrandon.91@gmail.com>
2023-09-04 09:38:27 -0700

    Add logic to generate values from required mold template variables

--- internal/mold/mold.go
+++ internal/mold/mold.go
@@ -1,8 +1,13 @@
 package mold
 
 import (
+	"bufio"
 	"errors"
+	"fmt"
 	"io"
+	"os"
+	"strconv"
+	"strings"
 
 	"gopkg.in/yaml.v3"
 )
@@ -11,6 +16,7 @@ var (
 	ErrMissingVariableName             = errors.New("missing environment variable name")
 	ErrEnvironmentVariableDoesNotExist = errors.New("environment variable does not exist")
 	ErrInvalidDataType                 = errors.New("value does not implement the required type")
+	ErrEmptyMold                       = errors.New("mold variables are empty")
 )
 
 const (
@@ -25,6 +31,10 @@ type MoldTemplateVariable struct {
 	Value    interface{} `yaml:"value"`
 	Type     string      `yaml:"type"`
 	Required bool        `yaml:"required"`
+}
+
+func (m *MoldTemplateVariable) String() string {
+	return fmt.Sprintf("%s = %v (type=%s, required=%t)", m.Name, m.Value, m.Type, m.Required)
 }
 
 // MoldTemplate data representation for the MoldTemplate
@@ -74,10 +84,84 @@ func New(r io.Reader) (*MoldTemplate, error) {
 	}, nil
 }
 
+// Generate runs the main logic to check for any required fields in the mold template and fills the values
+func (m *MoldTemplate) Generate() error {
+	if m.variables == nil {
+		return ErrEmptyMold
+	}
+
+	for k, v := range m.variables {
+		if !v.Required {
+			continue
+		}
+
+		if v.Value != "" && v.Value != nil {
+			fmt.Printf("'%s' is a required field, with the value of '%s'. Would you like to overwrite this value (yes/no)? ", v.Name, v.Value)
+			reader := bufio.NewReader(os.Stdin)
+			answer, err := reader.ReadString('\n')
+			if err != nil {
+				return err
+			}
+			answer = strings.TrimSpace(answer)
+			if answer == "no" || answer == "n" {
+				fmt.Println("Skipping...")
+				continue
+			}
+		}
+
+		fmt.Printf("Enter a value for %s (type=%s): ", v.Name, v.Type)
+		reader := bufio.NewReader(os.Stdin)
+		value, err := reader.ReadString('\n')
+		if err != nil {
+			return err
+		}
+		value = strings.TrimSpace(value)
+		switch v.Type {
+		case MoldDataTypeNumber:
+			if strings.Contains(value, ".") {
+				parsed, err := strconv.ParseFloat(value, 64)
+				if err != nil {
+					return err
+				}
+				v.Value = parsed
+			} else {
+				parsed, err := strconv.Atoi(value)
+				if err != nil {
+					return err
+				}
+				v.Value = parsed
+			}
+
+		case MoldDataTypeBoolean:
+			parsed, err := strconv.ParseBool(value)
+			if err != nil {
+				return err
+			}
+			v.Value = parsed
+
+		case MoldDataTypeString:
+			fallthrough
+		default:
+			v.Value = value
+		}
+		m.variables[k] = v
+	}
+	return nil
+}
+
 // GetVariable gets a MoldTemplateVariable by key. If the key does not exist an error will be returned.
 func (m *MoldTemplate) GetVariable(key string) (*MoldTemplateVariable, error) {
 	if v, ok := m.variables[key]; ok {
 		return &v, nil
 	}
 	return nil, ErrEnvironmentVariableDoesNotExist
+}
+
+// GetAllVariables returns all the variables in the mold
+func (m *MoldTemplate) GetAllVariables() []MoldTemplateVariable {
+	variables := []MoldTemplateVariable{}
+	for _, v := range m.variables {
+		variables = append(variables, v)
+	}
+	return variables
 }
